6efd6f2215f6e067eb16008db9d4139472840103
munhing <munhing1980@gmail.com>
2016-01-11 06:16:48 +0800

    minoe

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -2,7 +2,7 @@
 
 <div class="page-footer">
 	<div class="page-footer-inner">
-		 2014 &copy; Powered by LLPM.
+		 <?php echo date("Y") ?> &copy; Powered by LLPM.
 	</div>
 	<div class="scroll-to-top">
 		<i class="icon-arrow-up"></i>
