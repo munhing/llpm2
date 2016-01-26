f9e4a3450ce095ed7d1141f23855ce97faa83466
unknown <ks@BMLWS014.brighton.local>
2016-01-26 13:54:22 +0800

    recalculation

---
+++ app/LLPM/Containers/ContainerDaysCalculationByWorkOrder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace LLPM\Containers;
+
+use Activity;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+use LLPM\Containers\CalculateContainerDays;
+use LLPM\Repositories\ContainerRepository;
+
+class ContainerDaysCalculationByWorkOrder
+{
+
+    protected $containerRepository;
+    protected $calculateContainerDays;
+
+	function __construct(
+        ContainerRepository $containerRepository,
+        CalculateContainerDays $calculateContainerDays
+    )
+	{
+        $this->containerRepository = $containerRepository;
+        $this->calculateContainerDays = $calculateContainerDays;
+	}
+
+    /**
+     * Handle the command.
+     *
+     * @param object $command
+     * @return void
+     */
+    public function fire($workorder)
+    {
+        // 1. Obtain all active containers in the port, those with status 3
+        // $containers = $this->containerRepository->getAllActive();
+        dd($workorder);
+        
+        // $containers = $this->containerRepository->getSpecificContainer(181216);
+
+        // dd($containers->count());
+
+        // 2. Loop each container
+        foreach($containers as $container) {
+
+            // 3. Calculate container's days empty and laden
+            // this need to create a command of it's own because there will be other class needing this
+            // {"L":11,"E":0,"total":11}
+            Log::info("Calculating container#: $container->container_no");
+
+            $days = $this->calculateContainerDays->fire($container);
+
+            // dd($days);
+
+            Log::info(" => Done, updating to database...");
+            
+            // 4. Update DB
+            $this->containerRepository->updateDays($container->id, $days);
+            Log::info(" => Updated! \n");
+        }
+        
+
+   	
+	}    
+}
--- app/LLPM/WorkOrders/CalculateStorageChargesByWorkOrder.php
+++ app/LLPM/WorkOrders/CalculateStorageChargesByWorkOrder.php
@@ -5,26 +5,33 @@ namespace LLPM\WorkOrders;
 use Illuminate\Support\Facades\Log;
 use LLPM\Repositories\WorkOrderRepository;
 use LLPM\WorkOrders\CalculateStorageCharges;
+use LLPM\Containers\ContainerDaysCalculationByWorkOrder;
 use WorkOrder;
 
 class CalculateStorageChargesByWorkOrder		
 {
 	protected $calculateStorageCharges;
 	protected $workOrderRepository;
+	protected $$containerDaysCalculationByWorkOrder;
 
 	public function __construct(
 		CalculateStorageCharges $calculateStorageCharges,
-		WorkOrderRepository $workOrderRepository
+		WorkOrderRepository $workOrderRepository,
+		ContainerDaysCalculationByWorkOrder $containerDaysCalculationByWorkOrder
 	)
 	{
 		$this->calculateStorageCharges = $calculateStorageCharges;
 		$this->workOrderRepository = $workOrderRepository;
+		$this->$containerDaysCalculationByWorkOrder = $$containerDaysCalculationByWorkOrder;
 	}
 
 	public function fire($workorder)
 	{
 		// dd($workorder);
 		$workorder = $this->checkInstance($workorder);
+
+		//Should recalculate empty container days
+		$this->containerDaysCalculationByWorkOrder->fire($workorder);
 		
 		$storageCharges = $this->calculateStorageCharges->fire($workorder);
 
